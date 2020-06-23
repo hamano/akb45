73f9bb41a93c5d42fadac6e0a334f137b3443f1c
HAMANO Tsukasa <hamano@osstech.co.jp>
2020-06-23 23:04:23 +0900

    set keymap

--- keymaps/default/keymap.c
+++ keymaps/default/keymap.c
@@ -17,8 +17,7 @@
 
 // Defines names for use in layer keycodes and the keymap
 enum layer_names {
-    _BASE,
-    _FN
+    _BASE
 };
 
 // Defines the keycodes used by our macros in process_record_user
@@ -27,18 +26,19 @@ enum custom_keycodes {
     QMKURL
 };
 
+#define MT_CS MT(MOD_LCTL, KC_SPACE) // Tap for Space, hold for Control
+#define MT_LSS MT(MOD_LSFT, KC_SPACE) // Tap for Space, hold for LShift
+
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
-    /* Base */
     [_BASE] = LAYOUT(
-        KC_A,    KC_1,    MO(_FN),
-            KC_TAB,   KC_SPC
-    ),
-    [_FN] = LAYOUT(
-        QMKBEST, QMKURL,  _______,
-            RESET,    XXXXXXX
-    )
+  KC_ESC, KC_Q,   KC_W,   KC_E,   KC_R,   KC_T,   KC_Y,   KC_U,   KC_I,   KC_O,   KC_P,   KC_BSPC,
+  KC_LCTL,KC_A,   KC_S,   KC_D,   KC_F,   KC_G,   KC_H,   KC_J,   KC_K,   KC_L,   KC_SCLN,KC_ENT,
+  KC_LSFT,KC_Z,   KC_X,   KC_C,   KC_V,   KC_B,   KC_N,   KC_M,   KC_COMM,KC_DOT, KC_SLSH,KC_NO,
+  KC_NO,  KC_LALT,KC_LGUI,MT_CS,          MT_LSS,         KC_NO, KC_NO, KC_RALT, KC_NO
+	    )
 };
 
+/*
 bool process_record_user(uint16_t keycode, keyrecord_t *record) {
     switch (keycode) {
         case QMKBEST:
@@ -61,7 +61,6 @@ bool process_record_user(uint16_t keycode, keyrecord_t *record) {
     return true;
 }
 
-/*
 void matrix_init_user(void) {
 
 }
